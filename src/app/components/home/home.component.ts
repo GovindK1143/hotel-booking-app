943ad848eb6f18434ab547290c25c05e9faaa491
GovindK1143 <govind.satyam@gmail.com>
2024-02-22 22:59:45 +0530

    make some changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'], // Use styleUrls instead of styleUrl
 })
 export class HomeComponent {
+  featuredHotels: any[] = [
+    {
+      name: 'Hotel A',
+      location: 'City A',
+      price: '$100',
+      imageUrl: '',
+    },
+    {
+      name: 'Hotel B',
+      location: 'City B',
+      price: '$150',
+      imageUrl: '',
+    },
+    // Add more featured hotels as needed
+  ];
 
+  constructor() {}
+
+  searchHotels() {
+    // Implement search logic based on user input
+  }
 }
